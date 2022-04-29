67060a191fb2282c99f11300f64cb689ad91970a
luis <luisfelorivas@gmai.com>
2022-04-28 23:46:02 -0500

    add animation to the ball an collition detection in the bars

--- main.js
+++ main.js
@@ -56,6 +56,9 @@ class Ball{
         this.speed_x=3
         this.board=board
         this.direction=1
+        this.bounce_angle=0
+        this.max_bounce_angle=Math.PI/12
+        this.speed=3
 
         board.ball=this
         this.kind="circle"
@@ -64,6 +67,26 @@ class Ball{
     move(){
         this.x+=(this.speed_x*this.direction)
         this.y+=(this.speed_y)
+    }
+
+    collision(bar){
+        let relative_intersect_y=(bar.y+(bar.height/2))-this.y
+        let normalzed_intersect_y=relative_intersect_y/(bar.height/2)
+        this.bounce_angle=normalzed_intersect_y*this.max_bounce_angle
+        this.speed_y=this.speed*-Math.sin(this.bounce_angle)
+        this.speed_x=this.speed*Math.cos(this.bounce_angle)
+
+        if(this.x>(this.board.width/2)) this.direction=-1
+        else this.direction=1
+
+
+    }
+
+    get width(){
+        return this.radius*2
+    }
+    get height(){
+        return this.radius*2
     }
 }
 
@@ -114,13 +137,41 @@ class BoardView{
         if(this.board.playing){
             this.clean()
             this.draw()
+            this.check_collisions()
             this.board.ball.move()
 
         }
         
     }
+    check_collisions(){
+        for(let i=this.board.bars.length-1;i>=0;i--){
+            let bar=this.board.bars[i]
+            if(hit(bar,this.board.ball)){
+                this.board.ball.collision(bar)
+            }
+        }
+    }
 
 
+}
+function hit(a,b) {
+    let hit=false
+    if(b.x+b.width>=a&& b.x<a.x+a.width){
+        if(b.y+b.height>=a.y && b.y <a.y+a.height){
+            hit=true
+        }
+    }
+    if(b.x<=a.x&& b.x +b.width>=a.x+a.height){
+        if(b.y <=a.y && b.y +b.height>=a.y+a.height){
+            hit=true
+        }
+    }
+    if(a.x<=b.x&& a.x +a.width>=b.x+b.width){
+        if(a.y<=b.y&&a.y+a.height>=b.y+b.height){
+            hit=true
+        }
+    }
+    return hit
 }
 
 
@@ -153,7 +204,7 @@ document.addEventListener("keydown", e=> {
     console.log(e.key)
 })
 
-board_view.draw();
+board_view.draw();  
 window.requestAnimationFrame(controller)
 function controller() {
     board_view.play()
